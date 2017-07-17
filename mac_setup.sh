98c23e4d80196eedd19cee0994aa37c55f1cf10b
Angelos Panagiotopoulos <angelospanag@gmail.com>
2017-07-17 14:36:46 +0100

    Create mac_setup.sh

---
+++ mac_setup.sh
@@ -0,0 +1,48 @@
+#!bin/bash
+
+#Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Cakebrew
+brew cask install cakebrew
+
+#OhMyZsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+brew install wget
+
+#Git & Git config
+brew install git
+git config --global credential.helper osxkeychain
+git config --global user.email "angelospanag@gmail.com"
+git config --global user.name "Angelos Panagiotopoulos"
+git config --global commit.gpgsign true
+
+#Vim & Vim config
+brew install vim --with-override-system-vi
+echo "syntax on" >> ~/.vimrc
+echo "set nu" >> ~/.vimrc
+echo "colorscheme elflord" >> ~/.vimrc
+
+#I have horrible humour
+brew install fortune
+brew install cowsay
+echo "fortune | cowsay -f vader" >> ~/.zshrc
+
+#Go
+brew install go
+brew install glide
+brew install delve
+
+#Brew cask apps
+brew tap caskroom/fonts
+brew cask install atom
+brew cask install font-fira-code
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install keepassx
+brew cask install spectacle
+#brew cask install slack
+#brew cask install docker
+#brew cask install vagrant
+#brew cask install virtualbox
