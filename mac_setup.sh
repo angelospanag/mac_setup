b53bd8fcdafef5acb7586b18a57029d1c1936be9
Angelos Panagiotopoulos <angelospanag@gmail.com>
2017-11-22 11:58:27 +0000

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -98,6 +98,8 @@ brew install yarn
 
 # AWS
 brew install awscli
+sudo curl -o /usr/local/bin/ecs-cli https://s3.amazonaws.com/amazon-ecs-cli/ecs-cli-darwin-amd64-latest
+sudo chmod +x /usr/local/bin/ecs-cli
 
 # ApacheBench
 brew install homebrew/apache/ab
