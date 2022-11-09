7043e65138c3cd924b99c2e3655b26414f474b2c
Angelos Panagiotopoulos <angelospanag@protonmail.com>
2022-11-09 10:40:34 +0000

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -73,19 +73,26 @@ brew install --cask docker
 
 # Visual Studio Code and extensions
 brew install --cask visual-studio-code
-code --install-extension bungcip.better-toml
-code --install-extension dbaeumer.vscode-eslint
 code --install-extension golang.go
 code --install-extension hashicorp.terraform
 code --install-extension mechatroner.rainbow-csv
 code --install-extension mikestead.dotenv
 code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.black-formatter
+code --install-extension ms-python.isort
 code --install-extension ms-python.python
 code --install-extension ms-python.vscode-pylance
 code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
 code --install-extension njpwerner.autodocstring
 code --install-extension oderwat.indent-rainbow
 code --install-extension PKief.material-icon-theme
+code --install-extension rust-lang.rust-analyzer
+code --install-extension tamasfe.even-better-toml
+code --install-extension yzhang.markdown-all-in-one
 
 VS_CODE_SETTINGS="{\"workbench.colorTheme\": \"Default Dark+\",
         \"workbench.iconTheme\": \"material-icon-theme\",
