bac04ff30daf5ca02a18856f589c8dab8cf2b463
Angelos Panagiotopoulos <angelospanag@protonmail.com>
2025-08-11 00:26:51 +0100

    Update mac_setup.sh

--- mac_setup.sh
+++ mac_setup.sh
@@ -1,10 +1,7 @@
 # Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install btop cloc ffmpeg git jq nmap pwgen tree vim wget   
-
-# OhMyZsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew install btop cloc fd ffmpeg git jq lazygit neovim nmap pwgen starship ripgrep tree wget   
 
 # Remove annoying console login message
 touch ~/.hushlogin
@@ -15,18 +12,9 @@ ssh-keygen -t ed25519 -C "angelospanag@protonmail.com"
 # Who am I?
 echo "alias myip=\"curl ifconfig.me\"" >> ~/.zshrc
 
-# List preferred wireless networks
-echo "alias list-preferred-wireless-networks=\"networksetup -listpreferredwirelessnetworks en0\"" >> ~/.zshrc
-
 # Git & Git config
 git config --global user.email "angelospanag@protonmail.com"
 git config --global user.name "Angelos Panagiotopoulos"
-
-# Vim & Vim config
-echo "syntax on" >> ~/.vimrc
-echo "set nu" >> ~/.vimrc
-echo "colorscheme elflord" >> ~/.vimrc
-echo "set backspace=indent,eol,start" >> ~/.vimrc
 
 # Python
 brew install python@3.13 ipython ruff uv
@@ -34,16 +22,16 @@ echo "export PATH=\"/usr/local/opt/python@3.13/libexec/bin:\$PATH\"" >> ~/.zshrc
 source  ~/.zshrc
 
 # Go
-brew install go
+brew install go golangci-lint
 
 # Node.js
 brew install node@22 pnpm
 
 # Brew cask apps
-brew install --cask firefox iterm2 keepassxc krita ollama pycharm signal vlc
+brew install --cask goland iterm2 keepassxc ollama pycharm signal vlc
 
 # Gaming
-brew install --cask gog-galaxy steam whisky
+brew install --cask gog-galaxy steam
 
 # Docker
 brew install --cask docker
